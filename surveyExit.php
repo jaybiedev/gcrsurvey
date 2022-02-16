595eaaa80cecb40477a19a7d808920c401cd0fc5
jaybie <jaybiedev@gmail.com>
2022-02-16 14:43:30 -0600

    new

---
+++ surveyExit.php
@@ -0,0 +1,41 @@
+<?php
+    session_start();
+    session_destroy();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon_gcr.ico">
+    <link rel="stylesheet" href="css/survey.css">
+    <title>Thank You for your participation!</title>
+</head>
+<body>
+	<img class="hd_img" src="images/logo_globe_100px.png" height="auto" alt="logo">
+    <span class="head">
+	
+    Grace Communion Richardson Reaches out to You</span>
+	
+	<div id="survey">
+	
+        <span class="Morecondensed">This completes the questionnaire. Thank you so much for your participation!</span>
+
+		<br><br>
+        <div id="info">
+            <p>
+                <a href="https://gcrichardson.org" title="Grace Communion Richardson">Grace Communion Richardson</a> 
+                <br>201 N. Plano Road, Richardson, TX 75081 
+                <br>(830) 308-7284
+                <br>Church Services are on Sundays at 11:00 AM
+
+            </p>
+
+        </div>
+	
+	</div>
+	<video class="herobox" playsinline autoplay muted loop src="videos/GCR_Hero_2bit.mp4"></video>
+</body>
+</html>
